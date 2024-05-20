faf6d2abdaa6556fc30d23c89ec23c7c025abc5b
Zahra Sadeghi <thisiszahrasadeghi@gmail.com>
2024-05-20 20:35:22 +0330

    Update letterCounter.py

--- Day1/letterCounter.py
+++ Day1/letterCounter.py
@@ -1,2 +1,2 @@
-#this code will count the letter of the noun you give it
+#this code will count the letter of the name you give it
 print("your name has " + str(len(input("what is your name?"))) + " letters")
